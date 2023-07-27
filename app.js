a339364ef1adcdbd99c63b24fc517d95d8f9a085
Hega Cendikia Elanda <hegacendikia49@gmail.com>
2023-07-27 23:27:18 +0700

    add basic routes

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 9000;
+const routes = require("./routes");
 
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!", 200);
-});
-
-app.get("/user", (req, res) => {
-  res.send({ data: "Connected to express user" });
-});
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
